ec58bbceccb1c679c28db7887a1068c597fb0bc7
Mauro Ciancio <maurociancio@gmail.com>
2010-06-30 23:51:09 -0300

    funcion buscar adyacencias de un nodo

--- gps.lisp
+++ gps.lisp
@@ -1,3 +1,15 @@
+;adyacencias
+;((nodo1 (nodo2 nodo3)) (nodo2 (nodo1) ...))
+(defun adyacencias_de (nodo adyacencias)
+	(if (or (null nodo) (null adyacencias))
+		nil
+		(if (eq nodo (caar adyacencias))
+			(cadar adyacencias)
+			(adyacencias_de nodo (cdr adyacencias))
+		)
+	)
+)
+
 (defun find_path (source target esquinas adyacencias &optional(path '()))
 	(if (or (null source) (null target) (null esquinas) (null adyacencias))
 		nil
@@ -16,3 +28,8 @@
 ;=============================
 
 (test 'params (find_path nil nil nil nil) nil)
+
+;adyacencias
+(test 'ady1 (adyacencias_de 'a '((a (b c))) ) '(b c))
+(test 'ady2 (adyacencias_de 'a '((d (b c))) ) nil)
+(test 'ady3 (adyacencias_de 'a '((b (a c)) (a (b c))) ) '(b c))
